29ea7e7348bf74bd2dd9fa8512c2182df24afb41
Suraj Prajapati <31574333+sura-p@users.noreply.github.com>
2022-06-30 18:10:27 +0530

    Delete 2_supplychain directory

--- 2_supplychain/migrations/1_initial_migration.js
+++
@@ -1,9 +0,0 @@
-var ProductManagement = artifacts.require('Supply_Chain');
-var ChangeOwnership = artifacts.require('ownwership');
-
-module.exports = function(deployer) {
-  deployer.deploy(ProductManagement)
-  .then(function(){
-    return deployer.deploy(ChangeOwnership,ProductManagement.address);
-  })
-};
